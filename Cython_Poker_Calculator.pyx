f054c896c3836f999f2a3c5ab5a6809441b25094
Seán Blake <se.blake@hotmail.com>
2016-11-16 15:52:34 +0000

    Update Cython_Poker_Calculator.pyx

--- Cython_Poker_Calculator.pyx
+++ Cython_Poker_Calculator.pyx
@@ -417,7 +417,7 @@ def randpreflopodds(list hand, int x):
 	cdef tuple qqq, www
 	cdef int count, win
 
-	pool = ['2A', '3A', '4A', '5A', '6A', '7A', '8A', '9A', 'tA', 'jA', 'qA', 'kA', 'aA', '2B', '3B', '4B', '5B', '6B', '7B', '8B', '9B', 'tB', 'jB', 'qB', 'kB', 'aB', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'tC', 'jC', 'qC', 'kC', 'aC', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'tD', 'jD', 'qD', 'kD', 'aD']
+	pool = ['2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', 'tc', 'jc', 'qc', 'kc', 'ac', '2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', 'td', 'jd', 'qd', 'kd', 'ad', '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', 'th', 'jh', 'qh', 'kh', 'ah', '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', 'ts', 'js', 'qs', 'ks', 'as']
 	pool_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51]
 	blankhand, deletez = [], []
 	win, loss, count = 0, 0, 0
@@ -458,7 +458,6 @@ def randpreflopodds(list hand, int x):
 			continue
 
 	return (win/ float(count)), (loss/float(count)), ((win+loss)/float(count))
-
 
 
 
